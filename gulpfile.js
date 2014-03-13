d593d8b40c91cbb7c20fc08766e19e8609306b31
Isaac Chansky <ichansky@gmail.com>
2014-03-12 22:15:52 -0400

    adds simple gulp build

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var path = require('path');
+
+gulp.task('compile-grid', function () {
+  gulp.src('./less/main.less')
+    .pipe(less({
+      paths: [ path.join(__dirname, 'less', 'includes') ]
+    }))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('compile-display', function () {
+  gulp.src('./less/display.less')
+    .pipe(less({
+      paths: [ path.join(__dirname, 'less', 'includes') ]
+    }))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('default', ['compile-display', 'compile-grid']);
